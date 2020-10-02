9426546595938bcac80ab8cc03939f11d7f3a9b3
VictorBellver <bellver20952@iesmarenostrum.com>
2020-10-02 20:20:24 +0200

    Funciones
    Funciones y como debugearlas

---
+++ MyTest1/MyTest1/Geometry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTest1
+{
+    class Geometry
+    {
+        public static double GetVector2Dlength(double x, double y)
+        {
+            double aux;
+            double length;
+            aux = x * x + y * y;
+            length = Math.Sqrt(aux);
+            return length;
+        }
+        public static double GetVector3Dlength(double x, double y, double z)
+        {
+            double aux;
+            double length;
+            aux = x * x + y * y + z * z;
+            length = Math.Sqrt(aux);
+            return length;
+        }
+        
+        public static double GetCircleArea(double r)
+        {
+            double aux, pi;
+            pi = Math.PI;
+            aux = pi * (r * r);
+            return aux;
+
+        }
+
+        public static double GetSphereVolume(double r)
+        {
+            double aux;
+            aux = 4.0 * Math.PI * (r * r * r);
+            aux = aux / 3.0;
+            return aux;
+
+        }
+
+        public static double GetRetangleArea(double height, double width)
+        {
+            double aux;
+            aux = height * width;
+            return aux;
+
+        }
+    }  
+}
--- MyTest1/MyTest1/Program.cs
+++ MyTest1/MyTest1/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MyTest1
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Probar las soluciones para cada una de las formulas planteadas y terminar de configurar el puto Visual Studio
+
+            //Prueba Volumen de la esfera
+            double spVolume;
+            spVolume = Geometry.GetSphereVolume(4.0);
+
+            //Prueba Area Rectangulo
+            double recArea;
+            recArea = Geometry.GetRetangleArea(2.0, 4.0);
         }
     }
 }
